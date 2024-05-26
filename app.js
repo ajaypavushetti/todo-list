29983932b5157ad61e8176ddbcea4718b065ec8b
ajaypavushetti <159945869+ajaypavushetti@users.noreply.github.com>
2024-05-26 21:06:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-const express=require("express");
-const bodyparser=require("body-parser");
-const app=express();
-const port=3000;
-app.use(bodyparser.urlencoded({extended:true}));
-app.use(express.static("public")); 
-
-app.set('view engine','ejs');
-var inp=[];
-app.get("/",function(req,res){
-    res.render('index',{listitem:inp});
-})
-app.post("/",function(req,res){
- var ano= req.body.newinput;
- inp.push(ano);
-res.redirect("/");
-})
-app.post("/delete",function(req,res){
-    var del=req.body.index;
-    inp.splice(del,1);
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+let inp = [];
+function readDataFromFile() {
+        const data = fs.readFileSync("data.json", "utf8");
+        inp = JSON.parse(data);
+}
+function writeDataToFile() {
+        fs.writeFileSync("data.json", JSON.stringify(inp));
+}
+readDataFromFile();
+app.get("/", function(req, res) {
+    res.render('index', { listitem: inp });
+});
+app.post("/", function(req, res) {
+    var ano = req.body.newinput;
+    inp.push(ano);
+    writeDataToFile(); 
     res.redirect("/");
-})
-
-app.listen(port,function(){
-})
+});
+app.post("/delete", function(req, res) {
+    var index = req.body.index;
+    inp.splice(index, 1); 
+    writeDataToFile(); 
+    res.redirect("/");
+});
+app.listen(3000, function() {
+    
+});
