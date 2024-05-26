8ff01a3b749c7d4c8c76f8db3b274478a7735d3a
ajaypavushetti <159945869+ajaypavushetti@users.noreply.github.com>
2024-05-26 21:15:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,23 +10,19 @@ function readDataFromFile() {
         const data = fs.readFileSync("data.json", "utf8");
         inp = JSON.parse(data);
 }
-function writeDataToFile() {
-        fs.writeFileSync("data.json", JSON.stringify(inp));
-}
 readDataFromFile();
 app.get("/", function(req, res) {
     res.render('index', { listitem: inp });
 });
+
 app.post("/", function(req, res) {
     var ano = req.body.newinput;
     inp.push(ano);
-    writeDataToFile(); 
     res.redirect("/");
 });
 app.post("/delete", function(req, res) {
     var index = req.body.index;
     inp.splice(index, 1); 
-    writeDataToFile(); 
     res.redirect("/");
 });
 app.listen(3000, function() {
