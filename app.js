da1b2820aa4f2cbc7e35d5df99bc038a24bc522c
ajaypavushetti <159945869+ajaypavushetti@users.noreply.github.com>
2024-05-26 21:22:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const fs = require("fs");
 const app = express();
+const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static("public")); 
 app.set('view engine', 'ejs');
-let inp = [];
-function readDataFromFile() {
-        const data = fs.readFileSync("data.json", "utf8");
-        inp = JSON.parse(data);
-}
-readDataFromFile();
+var inp = [];
 app.get("/", function(req, res) {
     res.render('index', { listitem: inp });
 });
-
 app.post("/", function(req, res) {
     var ano = req.body.newinput;
     inp.push(ano);
-    res.redirect("/");
+    res.redirect("/"); 
 });
 app.post("/delete", function(req, res) {
     var index = req.body.index;
     inp.splice(index, 1); 
-    res.redirect("/");
+    res.redirect("/"); 
 });
-app.listen(3000, function() {
-    
+app.listen(port, function() {
+   
 });
